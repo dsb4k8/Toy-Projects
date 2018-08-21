2354ccfdcc40461ab31165e375ae7ee39524d2d1
Diego Brown <dsb4k8@gmail.com>
2018-08-21 17:47:19 -0500

    Updated __repr__ methods
    Whenever print is called on player object player's hand is displayed along with sum of ranks. JQK are treated as 10 if encountered. A is treated as a 11 if the current sum + 11 <= 21 otherwise is treated as 1. Demo of functionality located in main

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,16 @@
 from random import shuffle
 
 #make a deck of cards
-
+blocker = "***************"
 class card:
 	rank = "X"
 	suit = "X"
+
 	def __init__(self, rank, suit):
 		self.rank = rank
 		self.suit = suit
 	def __repr__(self):
 		return "{} of {}".format(self.rank, self.suit)
-	# def show(self):
-	# 	# print "{} of {}".format(self.rank, self.suit)
-	# 	print ("({} {})").format(self.rank, self.suit)
 
 class stack:
 	def __init__(self, cards):
@@ -27,30 +25,94 @@ class stack:
 			shuffle(self.contents)
 		return self.contents
 
-	def deal(self):
-		#deals out 5 cards
-		if len(self.contents) >= 5:
-			for i in range(20):
-				shuffle(self.contents)
-			self.contents = self.contents[:-5]
+	# def deal(self):
+	# 	#deal out 5 cards
+	# 	if len(self.contents) >= 2:
+	# 		for i in range(20):
+	# 			shuffle(self.contents)
+	# 		self.contents = self.contents[:-2]
 
-			hand = self.contents[-5:]
-			return hand
-		else:
-			print "< one hand remains in the deck. EMPTY"
+	# 		hand = self.contents[-2:]
+	# 		return hand
+	# 	else:
+	# 		print "< one hand remains in the deck. EMPTY"
 
+class player:
+	def __init__(self, WholeDeck):
+                self.hand = WholeDeck.contents[-2:]
+                
+        	WholeDeck.contents = WholeDeck.contents[:-2]
+		self.hit = self.hand.append(WholeDeck.top)
+	def __repr__(self):
+           return "%s, Total: %d " %(self.hand[:len(self.hand)-1], self.total())
+
+        def total(self):
+            result = 0
+            result_w_ace = ()
+            for n in self.hand[:len(self.hand)-1]:
+                if n.rank == "J" or n.rank == "Q" or n.rank == "K":
+                   result = result + 10
+                elif n.rank == "A":
+                    if result + 11 <= 21:
+                        result  = result + 11
+                    else:       
+                        result_w_ace  = (result + 1, result + 11)
+                        return result_w_ace
+                else:
+                    # print int(n.rank)
+                    result = result + int(n.rank)
+            return result
+            # return  int(self.hand[0].rank)
+
+
+
+
+
+
+		# return "%s" %(self.hand[0], self.hand[1])
+# class dealer(player):
+# 	def __init__(self,WholeDeck):
+# 		self.hand = WholeDeck.contents[-2:]
+# 		self.hit = self.hand.append(WholeDeck.top)
+# 	def __repr__(self):
+# 		return "{}, {}".format(self.hand[0], blocker)
+class dealer(player):
+        def __repr__(self):
+            return "{}".format(self.hand[0], blocker)
+
+
+            
 if __name__=="__main__":
 
 	ranks = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 	Suits = ["Hearts","Clubs","Diamonds","Spades"]
 
 	cardlist = []
+        empty = []
 
 	for suit in Suits:
 		for rank in ranks:
 			currentcard = card(rank, suit)
 			cardlist.append(currentcard)
+        play_deck = stack(cardlist)
+        discard_deck = stack(empty)
+        # print deck
+        play_deck.shuffle()
+        # print deck
+        p1 = player(play_deck)
+        print p1
+        print len(p1.hand)
 
-	deck = stack(cardlist)
-	print deck
+
+	# deck= stack(cardlist)
+        # deck.contents = cardlist
+	# print deck
+	# deck.shuffle()
+	# print
+	# print deck
+	# print 
+	# p1 = player(deck)
+        # print
+        # # print "p1's hand: {}".format(p1.hand)
+        # print p1.hand
 
